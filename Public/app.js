328b70b0f3d029d02e5126e0b8edf550b78b756d
Suri <109096878+Surisuri90@users.noreply.github.com>
2022-10-31 18:01:22 +0900

    Update app.js

--- Public/app.js
+++ Public/app.js
@@ -1,55 +1,28 @@
-window.addEventListener('load', function (){
-    console.log('page is loaded');
+window.addEventListener('load', ()=>{
 
-    let button = document.getElementById("disney-input');
+    //Request json file
+    fetch("app.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        let quotes = data.quotes;
+        let randomNum = Math.floor(Math.random() * quotes.length);
+        let randomQuote = quotes[randomNum];
+        document.getElementById("tart-quote").innerHTML = '"' + randomQuote + '"';
+    });
 
-    let button.addEventListener(('onchange'), function() {
-        let inputText = document.getElementById('disney-input').value;
-        console.log("This is clicked.");
+    //Request random pizza
+    let pizzaButton = document.getElementById("tart-button");
+    pizzaButton.addEventListener("click", () => {
+        console.log("Button clicked!");
 
-        fetch('app.json')
-            .then(response => response.json())
-            .then(data => {
-                let headingElement = document.getElementById('d-name');
-                
-                console.log("input text is ", inputText);
-                // console.log("This is my ", data);
-                // console.log("This is the size of my json: ", data.length);
-                
-                for(let i = 0; i < data.length; i++){
-                    // console.log(data[i].name);
-                    if (data[i].name === inputText){
-                        console.log("It's a match!");
-
-                        //What happens if it's a match
-                        let imageElement = document.getElementById('d-img');
-                        imageElement.src = data[i].url;
-
-                    } else {
-                        // console.log("It is not a match.");
-                        //What happen if it's not!
-                    }
-                }
-                //headingElement.innerHTML = birdArray[inputText].name;
-            })
-        
-        let frame = 0
-        let gonextButton = document.getElementById('go_next');
-        gonextButton.addEventListener('click', function () {
-            console.log('Woodi');
-            fetch('birdtry.json')
-                .then(Response => Response.json())
-                .then(data => {
-                    console.log(data);
-                    let birdArray = data.results;
-                    let imageElement = document.getElementById('imgp2');
-                    imageElement.src = birdArray[frame].url;
-                })
-            frame = (frame + 1) % 5;
-        })
-
-    })
-    const cursor = document.querySelector('.cursor');
-
-})})
-
+        //Request random pizza object
+        fetch("/random")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let tartString = "How about a " + data.type + " tart? It only costs $" + data.cost + "!";
+            document.getElementById("tart-random").innerHTML = tartString;
+        });
+    });
+});
